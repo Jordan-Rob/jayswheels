0f156fe4324bb602006b67c2eaea8cdf0874c318
jordanrob709 <jordanrob709@gmail.com>
2020-04-16 21:44:20 +0300

    added category detail

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CategoryList
+from .views import CategoryList, CategoryDetailView
 
 urlpatterns = [
     path('categories/', CategoryList.as_view(), name='categories'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category_detail')
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Category
 # Create your views here.
+
 
 class CategoryList(ListView):
     model = Category
     template_name = 'category_list.html'
 
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'category_detail.html'
