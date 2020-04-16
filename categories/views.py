9a169989fef50f88b602c350e9feab1e9b2c969c
jordanrob709 <jordanrob709@gmail.com>
2020-04-16 23:10:25 +0300

    added category detail

--- categories/views.py
+++ categories/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from .models import Category
+from cars.models import Car
 # Create your views here.
 
 
@@ -14,3 +15,10 @@ class CategoryList(ListView):
 class CategoryDetailView(DetailView):
     model = Category
     template_name = 'category_detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(CategoryDetailView, self).get_context_data(
+            *args, **kwargs)
+        context['category_cars'] = Car.objects.filter(
+            category=self.request.name)
+        return context
