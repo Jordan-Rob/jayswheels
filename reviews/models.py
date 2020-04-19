b757243c2615c093eb518e0fb22823b86da24068
jordanrob709 <jordanrob709@gmail.com>
2020-04-19 17:14:15 +0300

    added review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+
+from users.models import CustomUser
 
 # Create your models here.
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField(max_length=160)
+    date_added = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('review_detail', args=[str(self.id)])
