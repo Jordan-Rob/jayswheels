dd1f520731900f468f75ec2a79310066a5406879
jordanrob709 <jordanrob709@gmail.com>
2020-04-20 23:41:01 +0300

    added car_review templates

--- cars/urls.py
+++ cars/urls.py
@@ -4,5 +4,5 @@ from .views import CarDetailView, CarListView
 
 urlpatterns = [
     path('cars/', CarListView.as_view(), name='car_list'),
-    path('cars/<int:pk>', CarDetailView.as_view(), name='car_detail')
+    path('cars/<int:pk>', CarDetailView.as_view(), name='car_detail'),
 ]
--- cars/urls.py.c6a829fbad6b3d0515978e9ed7ef7313.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import CarDetailView, CarListView
-
-urlpatterns = [
-    path('cars/', CarListView.as_view(), name='car_list'),
-    path('cars/<int:pk>', CarDetailView.as_view(), name='car_detail')
-]
