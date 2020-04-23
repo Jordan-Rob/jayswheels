0c0a2bb74a7e032a661d334ed717ed69bb434311
jordanrob709 <jordanrob709@gmail.com>
2020-04-23 23:25:41 +0300

    added modifying review create url

--- cars/urls.py
+++ cars/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from .views import CarDetailView, CarListView
+from car_reviews.views import ReviewCreateView
 
 urlpatterns = [
     path('cars/', CarListView.as_view(), name='car_list'),
     path('cars/<int:pk>', CarDetailView.as_view(), name='car_detail'),
+    path('cars/<int:pk>/review/create/',
+         ReviewCreateView.as_view(), name='car_review_create')
 ]
